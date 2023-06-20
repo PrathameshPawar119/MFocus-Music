472ab2fc7739276f66106bc1ee61834f1bc1332f
Bhushan <alberteinstein02.99792458>
2023-06-20 20:46:38 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 let songs = [
   {
     songTitle: "Radha-Jale",
@@ -65,75 +64,84 @@ let songs = [
     songName: "Mangal Bhavan... Amangal Haari..",
     fileName: "songs/Ram_Siya_Ram.mp3",
     coverName: "song_banner/rama1.jpg",
-  }
+  },
+  {
+    songTitle: "Raam Siya Raam",
+    songName: "Mangal Bhavan... Amangal Haari..",
+    fileName: "songs/Ram_Siya_Ram.mp3",
+    coverName: "song_banner/rama1.jpg",
+  },
 ];
 
-let masterPlay = document.getElementById('masterPlay');
-// let songLoader = document.getElementById('songLoader');
+let masterPlay = document.getElementById("masterPlay");
+const nav = document.querySelector(".navbar");
 let songLoader = document.getElementsByClassName("songLoader");
-  songLoader = songLoader[0];
-let songGif = document.getElementById('songGif');
-let songItems = document.getElementsByClassName('songItem');
-let playThis = document.getElementsByClassName('playThis');
-let currentPlayingSong = document.getElementById('currentPlayingSong');
-let bottomSongMeta = document.getElementsByClassName('bottom_song_meta');
+songLoader = songLoader[0];
+let songGif = document.getElementById("songGif");
+let songItems = document.getElementsByClassName("songItem");
+let playThis = document.getElementsByClassName("playThis");
+let currentPlayingSong = document.getElementById("currentPlayingSong");
+let bottomSongMeta = document.getElementsByClassName("bottom_song_meta");
 let songIndex = 1;
 
-var audioElement = new Audio('songs/radhe_sham.mp3');
+window.addEventListener("scroll", function () {
+  let navHeight = nav.getBoundingClientRect().height;
+  let scrollHeight = this.window.pageYOffset;
+  if (scrollHeight > navHeight) {
+    nav.classList.add("fixedNav");
+  } else {
+    nav.classList.remove("fixedNav");
+  }
+});
 
+var audioElement = new Audio("songs/radhe_sham.mp3");
 
 // Putting Data into Cards
 for (let i = 0; i < songItems.length; i++) {
-    const element = songItems[i];
-    element.getElementsByTagName('img')[0].src = songs[i].coverName;
-    element.getElementsByTagName('h2')[0].innerText = songs[i].songTitle;
-    element.getElementsByTagName('p')[0].innerText = songs[i].songName;
-    
+  const element = songItems[i];
+  element.getElementsByTagName("img")[0].src = songs[i].coverName;
+  element.getElementsByTagName("h2")[0].innerText = songs[i].songTitle;
+  element.getElementsByTagName("p")[0].innerText = songs[i].songName;
 }
 
 //play button
-masterPlay.addEventListener('click', ()=>{
-    if (audioElement.paused) {
-        audioElement.play();
-    }
-    else{
-        audioElement.pause();
-    }
-    
+masterPlay.addEventListener("click", () => {
+  if (audioElement.paused) {
+    audioElement.play();
+  } else {
+    audioElement.pause();
+  }
 });
-
 
 // for each song's button
-Array.from(playThis).forEach((element) =>{
-    element.addEventListener('click', (e) => {
-            songIndex = parseInt(e.target.id);  // index of clicked song's button
-            audioElement.src = songs[songIndex-1].fileName;
-            currentPlayingSong.innerText = songs[songIndex-1].songTitle;
-            audioElement.currentTime = 0;
-            audioElement.play();
-
-            masterPlay.style.clipPath =
-              "polygon(0 0, 40% 0, 40% 100%, 60% 100%, 60% 0, 100% 0, 100% 100%, 0 100%);";
-    });
-});
-
-
-// Previous and Next butons eventlisteners
-let previous = document.getElementById('previousBtn');
-let next = document.getElementById('nextBtn');
-
-next.addEventListener('click', ()=> {
-    songIndex = ((songIndex) % songs.length)+1;
+Array.from(playThis).forEach((element) => {
+  element.addEventListener("click", (e) => {
+    songIndex = parseInt(e.target.id); // index of clicked song's button
     audioElement.src = songs[songIndex - 1].fileName;
     currentPlayingSong.innerText = songs[songIndex - 1].songTitle;
     audioElement.currentTime = 0;
     audioElement.play();
-})
+
+    masterPlay.style.clipPath =
+      "polygon(0 0, 40% 0, 40% 100%, 60% 100%, 60% 0, 100% 0, 100% 100%, 0 100%);";
+  });
+});
+
+// Previous and Next butons eventlisteners
+let previous = document.getElementById("previousBtn");
+let next = document.getElementById("nextBtn");
+
+next.addEventListener("click", () => {
+  songIndex = (songIndex % songs.length) + 1;
+  audioElement.src = songs[songIndex - 1].fileName;
+  currentPlayingSong.innerText = songs[songIndex - 1].songTitle;
+  audioElement.currentTime = 0;
+  audioElement.play();
+});
 previous.addEventListener("click", () => {
   if (songIndex <= 0) {
     songIndex = 0;
-  }
-  else{
+  } else {
     songIndex -= 1;
   }
   audioElement.src = songs[songIndex - 1].fileName;
@@ -142,29 +150,21 @@ previous.addEventListener("click", () => {
   audioElement.play();
 });
 
-
-
-
 // Progress bar
-audioElement.addEventListener('timeupdate', ()=>{
-    // Update seek bar here
-    progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
-    songLoader.value = progress;
+audioElement.addEventListener("timeupdate", () => {
+  // Update seek bar here
+  progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
+  songLoader.value = progress;
 });
 
-songLoader.addEventListener('change', ()=>{
-    audioElement.currentTime = (songLoader.value*audioElement.duration)/100;
-})
-
-
-
+songLoader.addEventListener("change", () => {
+  audioElement.currentTime = (songLoader.value * audioElement.duration) / 100;
+});
 
 // Function to check that audio is playing or not at instance
-function isPlaying(audioElement) 
-{ 
-  return !audioElement.paused; 
+function isPlaying(audioElement) {
+  return !audioElement.paused;
 }
-
 
 // // Import the functions you need from the SDKs you need
 // import { initializeApp } from "firebase/app";
@@ -187,5 +187,3 @@ function isPlaying(audioElement)
 // // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
-
-
