50918f70c9eb7f70459ebfb7478c0bdbc08c7e09
prathamesh-futurecraft <prathamesh@futurecraft.ing>
2025-12-14 19:34:35 +0530

    new songs

---
+++ db.js
@@ -0,0 +1,147 @@
+// Dynamic Songs Structure by Folders
+const songsByFolder = {
+    'LowFi': [
+      {
+        songTitle: "Radha-Jale",
+        songName: " Radha kaise na jale ~ lofi bits",
+        fileName: "songs/Radha_jale.mp3",
+        coverName: "song_banner/govind-radhe.jpg",
+        folder: "LowFi"
+      },
+      {
+        songTitle: "Radhe-sham",
+        songName: " Govind Radhe Radhe sham ~ lofi bits",
+        fileName: "songs/radhe_sham.mp3",
+        coverName: "song_banner/krishna1.jpg",
+        folder: "LowFi"
+      },
+      {
+        songTitle: "Radha Rani",
+        songName: "Radha Rani Lage ~ bass reaxed.",
+        fileName: "songs/radha_rani.mp3",
+        coverName: "song_banner/radha1.png",
+        folder: "LowFi"
+      },
+     
+      {
+        songTitle: "Ham Katha Sunate",
+        songName: "Ham katha sunate ram shakal gun dham ki ....",
+        fileName: "songs/ham_katha_sunate.mp3",
+        coverName: "song_banner/rama2.jpg",
+        folder: "LowFi"
+      },
+      {
+        songTitle: "Aarambh Hei Prachand",
+        songName: "Aarambh hei Prachand ~ Bass Boosted..",
+        fileName: "songs/aarambh_he_prachand.mp3",
+        coverName: "song_banner/rama3.jpg",
+        folder: "LowFi"
+      },
+      {
+        songTitle: "Hanuman Chalisa",
+        songName: "Hanuman-Chalisa ~ bas boosted",
+        fileName: "songs/hanuman_chalisa.mp3",
+        coverName: "song_banner/hanumana1.jpg",
+        folder: "LowFi"
+      }
+    ],
+    'motivations': [
+      {
+        songTitle: "Raam Siya Raam",
+        songName: "Mangal Bhavan... Amangal Haari..",
+        fileName: "songs/Ram_Siya_Ram.mp3",
+        coverName: "song_banner/rama1.jpg",
+        folder: "motivations"
+      },
+      {
+        songTitle: "Hey Raam",
+        songName: "Hey Raam (Female Version) ",
+        fileName: "songs/Hey_Ram(female).mp3",
+        coverName: "song_banner/raam-siya1.jpg",
+        folder: "motivations"
+      },
+      {
+        songTitle: "Hey Raam",
+        songName: "Hey Raam (Male Version) ",
+        fileName: "songs/Hey_Ram(male).mp3",
+        coverName: "song_banner/rama4.jpg",
+        folder: "motivations"
+      },
+      {
+        songTitle: "Namo Namo",
+        songName: "Namo Namo ji shankara (Kedarnath)",
+        fileName: "songs/namo_namo.mp3",
+        coverName: "song_banner/shiva1.jpg",
+        folder: "motivations"
+      },
+      {
+        songTitle: "Kahani Karn ki",
+        songName: "My Favourite",
+        fileName: "songs/Kahani_Karn_ki.mp3",
+        coverName: "song_banner/karna1.png",
+        folder: "motivations"
+      }
+    ],
+    'Marathi Love': [
+      {
+        songTitle: "Saazni",
+        songName: "Saazni ~ Marathi Love Song",
+        fileName: "songs/Saazni (Official Video) (1).mp3",
+        coverName: "song_banner/saaznisong.jpg",
+        folder: "Marathi Love"
+      },
+      {
+        songTitle: "Saaz yo tuza..",
+        songName: "Saaz yo tuza. marathi love song",
+        fileName: "songs/Saaj-Hyo-Tuza.mp3",
+        coverName: "song_banner/saaz hyu tuza.jpg",
+        folder: "Marathi Love"
+      },
+      {
+        songTitle: "Man Dhavataya",
+        songName: "Man Dhavtaya Tuzyach mage... (My Favourite)",
+        fileName: "songs/Mann_Dhaavataya.mp3",
+        coverName: "song_banner/man dhavtaya.jpg",
+        folder: "Marathi Love"
+      }
+    ],
+    'Upgraded Old Love': [
+      {
+        songTitle: "Hawa Hawa GenZ Version",
+        songName: "Hawa Hawa - from Dhurandhar - The JINN",
+        fileName: "songs/hawahawa.mp3",
+        coverName: "song_banner/jinnnnn.webp",
+        folder: "Upgraded Old Love"
+      },
+      {
+        songTitle: "Ishq Jalakar - Kaarvan",
+        songName: "Ishq Jalakar - Kaarvan - Dhurandhar",
+        fileName: "songs/ishq_kaarvan.mp3",
+        coverName: "song_banner/ishq_jalakar_karvan.jpg",
+        folder: "Upgraded Old Love"
+      },
+      {
+        songTitle: "Run down the city X Monika",
+        songName: "Run down the city X Monika - Dhurandhar",
+        fileName: "songs/run_down_city_monika.mp3",
+        coverName: "song_banner/rundown_monika.jpg",
+        folder: "Upgraded Old Love"
+      },
+    ],
+    'Rebel': [
+      {
+        songTitle: "Dhurandhar Title track",
+        songName: "Na de dil pardesi nu...",
+        fileName: "songs/dhurandhar_title_track.mp3",
+        coverName: "song_banner/dhurandhar_title.webp",
+        folder: "Rebel"
+      },
+      {
+        songTitle: "Fa9la Rebel entry",
+        songName: "Fa9la Rebel entry song",
+        fileName: "songs/fa9la_rahman_dakt.mp3",
+        coverName: "song_banner/fa9la.webp",
+        folder: "Rebel"
+      }
+    ]
+  };
